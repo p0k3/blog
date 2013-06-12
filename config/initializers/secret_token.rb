3e89e63572ff6e0b2d030ad038c0be70c0c8cccb
Paul Gruson <pgruson@qualimetrie.com>
2013-06-12 09:34:17 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '118791b7843c0382207a65ecdd04bb6d746adbcf8d7819ed1c067c3cea38bcbaa1127dae2fa47a543a6762ad2b738fd539695ac04ea8bf985acf227a6e4db90c'
